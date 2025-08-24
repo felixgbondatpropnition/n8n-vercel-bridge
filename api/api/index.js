f58cbb92e67a0af87d97d75434577ddf767c3830
felixgbondatpropnition <felixgbond@gmail.com>
2025-08-24 19:27:36 +0100

    Delete api/api directory

--- api/api/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    status: 'n8n Vercel Bridge Active',
-    endpoints: [
-      '/api/n8n',
-      '/api/n8n?action=list_workflows'
-    ]
-  });
-};
